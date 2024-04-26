ee6efce3e66923be2b8b2c3e34f0828a6dd3fd4c
HardsonUMN <161796680+HardsonUMN@users.noreply.github.com>
2024-04-26 18:25:16 +0700

    Update main.py

--- folder code mix/main.py
+++ folder code mix/main.py
@@ -1,13 +1,12 @@
 from machine import Pin, PWM, Timer
 import utime
-
-# Define GPIO pins for encoder channels
-encoder_channel_A = 8
-encoder_channel_B = 9
+from rotary_irq_rp2 import RotaryIRQ
+vb
+rotary = RotaryIRQ(10, 11)
 
 # Define driver pin
-RPWM = 6
-LPWM = 7
+RPWM = 14
+LPWM = 15
 
 # Set up the GPIO pins as outputs
 RPWM_pin = Pin(RPWM, Pin.OUT)
@@ -21,58 +20,25 @@ LPWM_pwm.freq(1000)
 RPWM_pwm.duty_u16(0)
 LPWM_pwm.duty_u16(0)
 
-# Setup GPIO pins as inputs with pull-down resistors
-encoder_A = Pin(encoder_channel_A, Pin.IN, Pin.PULL_DOWN)
-encoder_B = Pin(encoder_channel_B, Pin.IN, Pin.PULL_DOWN)
-
 # Initialize variables to keep track of encoder state
-encoder_position = 0
-prev_state_A = encoder_A.value()
-prev_state_B = encoder_B.value()
-
 def stop_motors():
     RPWM_pwm.duty_u16(0)
     LPWM_pwm.duty_u16(0)
-    
-def encoder():
-    global encoder_position, prev_state_A, prev_state_B
-    # Read the current state of the encoder channels
-    current_state_A = encoder_A.value()
-    current_state_B = encoder_B.value()
         
-    # Check for changes in encoder state
-    if current_state_A != prev_state_A or current_state_B != prev_state_B:
-        # Encoder A has changed, update position
-        if current_state_A == 1 and prev_state_A == 0 and current_state_A > current_state_B:
-            encoder_position -= 1
-                
-        elif current_state_B == 1 and prev_state_B == 0 and current_state_A < current_state_B:
-            encoder_position += 1
-
-    # Update encoder state variables
-    prev_state_A = current_state_A
-    prev_state_B = current_state_B
-
-    # Print the current encoder position and encoder values
-    print("Encoder Position: {}, Encoder Values: A={}, B={}".format(encoder_position, current_state_A, current_state_B))
-
-    
 def drive_motor(R,L):
     RPWM_pwm.duty_u16(R*256)
     LPWM_pwm.duty_u16(L*256)
 
 def callbacks(timer):
     stop_motors()
-
-timer = Timer()
-try:
-    drive_motor(0,150)
-    timer.init(mode=Timer.ONE_SHOT, period=5000, callback=callbacks)
-    while True:
-        encoder()
-        # Add a small delay to control the speed of reading
-        utime.sleep_ms(10)
-
-except KeyboardInterrupt:
-    stop_motors()
-    pass
+    print("end")
+    
+current_val = 0  # Track the last known value of the encoder
+drive_motor(100, 0)
+while True:
+    new_val = rotary.value()  # What is the encoder value right now?
+    
+    if current_val != new_val:  # The encoder value has changed!
+        print('Encoder value:', new_val)  # Do something with the new value
+        
+        current_val = new_val  # Track this change as the last know value
